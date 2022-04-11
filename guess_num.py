570a0adcf6d0d9662e0a9c28b97e6a593cc822a1
Alan Tu <tu19970201@gmail.com>
2022-04-11 23:55:06 +0800

    add count

--- guess_num.py
+++ guess_num.py
@@ -1,10 +1,12 @@
 import random
 
 r = random.randint(1, 100)
+t = 0
 
 while True:
     guess = input('猜數字1~100:')
     guess = int(guess)
+    t += 1
 
     if guess > r:
         print('小一點')
@@ -13,5 +15,8 @@ while True:
         print('大一點')
 
     else:
-        print('終於猜對了！')
+        print('終於猜對了！')    
+        print('你猜了第', t, '次')
         break
+        
+    print('你猜了第', t, '次')
